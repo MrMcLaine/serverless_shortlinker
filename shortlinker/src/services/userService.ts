b5a1937315ee375f4e1b91c16388644ff9b2aa4c
MrMcLaine <Lypovskyi@gmail.com>
2023-11-07 01:14:05 +0200

    feat/ add login lamda-function

--- shortlinker/src/services/userService.ts
+++ shortlinker/src/services/userService.ts
@@ -1,7 +1,7 @@
 import { v4 as uuid } from 'uuid';
 import { IUser } from '../interfaces/IUser';
 import dynamoDb from '../libs/db';
-import { createPasswordHash } from '../utils/passwordUtil';
+import {comparePassword, createPasswordHash} from '../utils/passwordUtil';
 import { authService } from './AuthService';
 
 class UserService {
@@ -25,6 +25,43 @@ class UserService {
         const token = await authService.generateToken({ userId });
 
         return { userId, token };
+    }
+
+    async login(email: string, password: string): Promise<{ userId: string; token: string }> {
+        const user = await this.getUserByEmail(email);
+
+        if (!user) {
+            throw new Error('User does not exist or wrong password.');
+        }
+
+        const passwordIsValid = await comparePassword(password, user.passwordHash);
+
+        if (!passwordIsValid) {
+            throw new Error('User does not exist or wrong password.');
+        }
+
+        const userId = user.userId;
+
+        const token = await authService.generateToken({ userId: user.userId });
+        return { userId, token };
+    }
+
+    private async getUserByEmail(email: string): Promise<IUser | null> {
+        const params = {
+            TableName: process.env.USERS_TABLE!,
+            KeyConditionExpression: 'email = :email',
+            ExpressionAttributeValues: {
+                ':email': email,
+            },
+        };
+
+        const result = await dynamoDb.query(params).promise();
+
+        if (result.Items && result.Items.length > 0) {
+            return result.Items[0] as IUser;
+        }
+
+        return null;
     }
 }
 
